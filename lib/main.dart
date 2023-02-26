b32372f68833ea07a48ca05358f332394358365f
Julian <julxott@outlook.de>
2023-02-26 10:11:51 +0100

    feat: [#1] Add i18n support
    * Add dependency
    * Add german and english translations
    * Configure dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
 
-  runApp(const FlinkApp());
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [
+        Locale('en', 'US'),
+        Locale('de', 'DE'),
+      ],
+      path: 'assets/i18n',
+      fallbackLocale: const Locale('en', 'US'),
+      child: const FlinkApp(),
+    ),
+  );
 }
 
 class FlinkApp extends StatelessWidget {
@@ -15,6 +27,9 @@ class FlinkApp extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flink'),
